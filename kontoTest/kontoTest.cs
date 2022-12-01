b483068ad64ffe0a9b0c5f7618e05a9f22d4ddb7
janpaul.w <janpaul.w@gso.schule.koeln>
2022-12-01 11:15:56 +0100

    Test für Buchungen erstellt

--- kontoTest/kontoTest.cs
+++ kontoTest/kontoTest.cs
@@ -18,5 +18,26 @@ namespace BankTest
             //Assert
             Assert.AreEqual(startguthaben, k.Guthaben);
         }
+        [TestMethod]
+        public void Konto_KannBuchungenVornehmen()
+        {
+            //Arrange 
+            Konto k;
+            int Anfangsguthaben = 100;
+            int Einzahlbetrag = 10;
+            int Auszahlbetrag = 10;
+            int Ziehlguhaben = 120;
+
+            //Act
+            k = new Konto(Anfangsguthaben);
+            for (int i = 0; i < 3; i++)
+            {
+                k.Einzahlen(Einzahlbetrag);
+            }
+            k.Auszahlen(Auszahlbetrag);
+
+            //Assert
+            Assert.AreEqual(120, Ziehlguhaben);
+        }
     }
 }
