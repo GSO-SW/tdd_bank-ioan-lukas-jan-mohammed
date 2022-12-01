acc6061552803262d9be2dbcc9bc0c3ccec3f6d3
janpaul.w <janpaul.w@gso.schule.koeln>
2022-12-01 11:02:28 +0100

    Arrange, Act, Asserts hinzugefügt

--- kontoTest/kontoTest.cs
+++ kontoTest/kontoTest.cs
@@ -1,12 +1,22 @@
-namespace kontoTest
+using Bank;
+
+namespace BankTest
 {
     [TestClass]
-    public class kontoTest
+    public class BankTest
     {
         [TestMethod]
         public void TestMethod1()
         {
+            //Arrange
+            Konto k;
+            int startguthaben = 100;
 
+            //Act
+            k = new Konto(startguthaben);
+
+            //Assert
+            Assert.AreEqual(startguthaben, k.Guthaben);
         }
     }
 }
