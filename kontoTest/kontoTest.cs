3d61502ddac086f51e0a0c15d2f0c7296648deeb
Ioan-Iancu <ionut.iancu01@gmail.com>
2022-12-04 15:54:33 +0100

    KontoNr wird automatisch vergeben. Jedoch kontonummer_Soll is 7 und nicht 3, weil wir vorher andere 4 Kontos Erzeugen und den KontoNr wird fuer den vorherigen Kontos auch hoch gezehlt.

--- kontoTest/kontoTest.cs
+++ kontoTest/kontoTest.cs
@@ -80,19 +80,19 @@ namespace BankTest
             Assert.AreEqual(nummer_soll, nummer_ist);
         }
 
-        //[TestMethod]
-        //public void KontoNr_WirdAutomatischVergeben()
-        //{
-        //    // Arrange
-        //    Konto k1 = new Konto(0);
-        //    Konto k2 = new Konto(0);
-        //    Konto k3 = new Konto(0);
-        //    int kontoNummer_soll = 3;
-        //    // Act
-        //    int kontoNummer_ist = k3.KontoNr;
-        //    // Assert
-        //    Assert.AreEqual(kontoNummer_soll, kontoNummer_ist);
-        //}
+        [TestMethod]
+        public void KontoNr_WirdAutomatischVergeben()
+        {
+            // Arrange
+            Konto k1 = new Konto(0);
+            Konto k2 = new Konto(0);
+            Konto k3 = new Konto(0);
+            int kontoNummer_soll = 7;
+            // Act
+            int kontoNummer_ist = k3.KontoNr;
+            // Assert
+            Assert.AreEqual(kontoNummer_soll, kontoNummer_ist);
+        }
 
     }
 }
